8581f34c1a2a51ff2ce2e45fc15833ef65ca07c0
Nii Yeboah <nii@vaadin.com>
2019-08-22 14:15:17 +0300

    Change io-dialog to use vaadin-confirm-dialog

--- src/components/vcf-network-io-dialog.js
+++ src/components/vcf-network-io-dialog.js
@@ -1,6 +1,7 @@
 import { html, PolymerElement } from '@polymer/polymer/polymer-element';
 import { ThemableMixin } from '@vaadin/vaadin-themable-mixin/vaadin-themable-mixin';
 import { Edge } from '../util/vcf-network-shared';
+import '@vaadin/vaadin-confirm-dialog';
 
 class VcfNetworkIODialog extends ThemableMixin(PolymerElement) {
   static get template() {
@@ -32,10 +33,6 @@ class VcfNetworkIODialog extends ThemableMixin(PolymerElement) {
           min-width: 400px;
         }
 
-        .content {
-          padding: var(--lumo-space-m) var(--lumo-space-l);
-        }
-
         .button-container {
           background-color: var(--lumo-shade-5pct);
           padding: var(--lumo-space-s) var(--lumo-space-l);
@@ -47,59 +44,16 @@ class VcfNetworkIODialog extends ThemableMixin(PolymerElement) {
           min-width: 90px;
           margin-left: var(--lumo-space-m);
         }
-
-        .content {
-          display: flex;
-        }
-
-        .content h6 {
-          margin: 0 auto 0 0;
-        }
-
-        .arrow-right {
-          margin: auto var(--lumo-space-l);
-        }
-
-        .arrow-right::before {
-          content: '';
-          width: 20px;
-          height: 0;
-          border-top: 1px solid var(--lumo-shade-10pct);
-          border-bottom: 1px solid var(--lumo-shade-10pct);
-          display: inline-block;
-          margin-bottom: 9px;
-        }
-
-        .arrow-right::after {
-          content: '';
-          width: 0;
-          height: 0;
-          border-top: 10px solid transparent;
-          border-bottom: 10px solid transparent;
-          border-left: 10px solid var(--lumo-shade-20pct);
-          display: inline-block;
-          margin: auto;
-        }
       </style>
-      <div id="backdrop" class="backdrop">
-        <div class="dialog">
-          <div class="content" id="content">
-            <div>
-              <h6>From</h6>
-              <div id="from"></div>
-            </div>
-            <div class="arrow-right"></div>
-            <div>
-              <h6>To</h6>
-              <div id="to"></div>
-            </div>
-          </div>
-          <div class="button-container">
-            <vaadin-button theme="primary" id="save">Save</vaadin-button>
-            <vaadin-button theme="tertiary" id="cancel">Cancel</vaadin-button>
-          </div>
-        </div>
-      </div>
+      <vaadin-confirm-dialog id="dialog" cancel confirm-text="Save">
+        <vcf-network-io-dialog-content id="content"></vcf-network-io-dialog-content>
+        <vaadin-button id="save" slot="confirm-button" theme="primary">
+          Save
+        </vaadin-button>
+        <vaadin-button id="cancel" slot="cancel-button" theme="tertiary">
+          Cancel
+        </vaadin-button>
+      </vaadin-confirm-dialog>
     `;
   }
 
@@ -131,7 +85,6 @@ class VcfNetworkIODialog extends ThemableMixin(PolymerElement) {
   connectedCallback() {
     super.connectedCallback();
     this.$.cancel.addEventListener('click', () => this.close());
-    this.$.backdrop.addEventListener('click', e => e.target.id === 'backdrop' && this.close());
     this.$.save.addEventListener('click', () => {
       if (this.isValid) {
         const edge = new Edge({ from: this.fromNode.id, to: this.toNode.id });
@@ -157,6 +110,13 @@ class VcfNetworkIODialog extends ThemableMixin(PolymerElement) {
         this.close();
       }
     });
+    var style = document.createElement('style');
+    style.innerHTML = `
+      #target {
+      color: blueviolet;
+      }
+    `;
+    document.head.appendChild(style);
   }
 
   get isValid() {
@@ -168,45 +128,64 @@ class VcfNetworkIODialog extends ThemableMixin(PolymerElement) {
   }
 
   open() {
-    this.style.opacity = 1;
-    this.style.display = 'block';
+    if (this._autoSelectInput && this._autoSelectOutput) {
+      this.$.save.click();
+    } else {
+      this.$.dialog.opened = true;
+      const overlay = document.querySelector('vaadin-dialog-overlay');
+      const overlayPart = overlay.shadowRoot.querySelector('[part="overlay"]');
+      overlayPart.style.width = '510px';
+    }
   }
 
   close() {
-    this.style.opacity = 0;
+    this.$.dialog.opened = false;
     setTimeout(() => {
-      this.$.to.innerHTML = '';
-      this.$.from.innerHTML = '';
+      this.$.content.$.to.innerHTML = '';
+      this.$.content.$.from.innerHTML = '';
       this.fromNode = null;
       this.toNode = null;
-      this.style.display = 'none';
     }, 200);
   }
 
   _toNodeChanged(toNode) {
     if (toNode) {
+      this._autoSelectInput = false;
       this.toComponent = toNode.type === 'component';
       if (this.toComponent) {
         const inputs = toNode.nodes.filter(node => node.type === 'component' || node.type === 'input');
-        this._createSelect(this.$.to, inputs, 'input').label = toNode.label;
+        const inputSelect = this._createSelect(this.$.content.$.to, inputs, 'input');
+        inputSelect.label = toNode.label;
+        if (inputs.length === 1 && inputs[0].type === 'input') {
+          inputSelect.value = inputs[0];
+          this._autoSelectInput = true;
+        }
       } else {
         const parent = this.main.context ? this.main.context.component.label : 'Root';
         this._activeInputSelect = { value: this.toNode };
-        this._createSelect(this.$.to, null, 'input', false).label = parent;
+        this._createSelect(this.$.content.$.to, null, 'input', false).label = parent;
+        this._autoSelectInput = true;
       }
     }
   }
 
   _fromNodeChanged(fromNode) {
     if (fromNode) {
+      this._autoSelectOutput = false;
       this.fromComponent = fromNode.type === 'component';
       if (this.fromComponent) {
-        const inputs = fromNode.nodes.filter(node => node.type === 'component' || node.type === 'output');
-        this._createSelect(this.$.from, inputs, 'output').label = fromNode.label;
+        const outputs = fromNode.nodes.filter(node => node.type === 'component' || node.type === 'output');
+        const outputSelect = this._createSelect(this.$.content.$.from, outputs, 'output');
+        outputSelect.label = fromNode.label;
+        if (outputs.length === 1 && outputs[0].type === 'output') {
+          outputSelect.value = outputs[0];
+          this._autoSelectOutput = true;
+        }
       } else {
         const parent = this.main.context ? this.main.context.component.label : 'Root';
         this._activeOutputSelect = { value: this.fromNode };
-        this._createSelect(this.$.from, null, 'output', false).label = parent;
+        this._createSelect(this.$.content.$.from, null, 'output', false).label = parent;
+        this._autoSelectOutput = true;
       }
     }
   }
@@ -275,7 +254,7 @@ class VcfNetworkIODialog extends ThemableMixin(PolymerElement) {
 
   _getDeepPath(type) {
     let path = [];
-    const content = type === 'input' ? this.$.to : this.$.from;
+    const content = type === 'input' ? this.$.content.$.to : this.$.content.$.from;
     const component = type === 'input' ? this.toNode : this.fromNode;
     if (this.main.context) path = this.main.contextStack.map(context => context.component.id);
     path.push(component.id);
@@ -288,3 +267,60 @@ class VcfNetworkIODialog extends ThemableMixin(PolymerElement) {
 }
 
 customElements.define(VcfNetworkIODialog.is, VcfNetworkIODialog);
+
+class VcfNetworkIODialogContent extends ThemableMixin(PolymerElement) {
+  static get template() {
+    return html`
+      <style include="lumo-typography">
+        :host {
+          display: flex;
+          min-width: 400px;
+        }
+
+        h6 {
+          margin: 0 auto 0 0;
+        }
+
+        .arrow-right {
+          margin: auto var(--lumo-space-l);
+        }
+
+        .arrow-right::before {
+          content: '';
+          width: 20px;
+          height: 0;
+          border-top: 1px solid var(--lumo-shade-10pct);
+          border-bottom: 1px solid var(--lumo-shade-10pct);
+          display: inline-block;
+          margin-bottom: 9px;
+        }
+
+        .arrow-right::after {
+          content: '';
+          width: 0;
+          height: 0;
+          border-top: 10px solid transparent;
+          border-bottom: 10px solid transparent;
+          border-left: 10px solid var(--lumo-shade-20pct);
+          display: inline-block;
+          margin: auto;
+        }
+      </style>
+      <div>
+        <h6>From</h6>
+        <div id="from"></div>
+      </div>
+      <div class="arrow-right"></div>
+      <div>
+        <h6>To</h6>
+        <div id="to"></div>
+      </div>
+    `;
+  }
+
+  static get is() {
+    return 'vcf-network-io-dialog-content';
+  }
+}
+
+customElements.define(VcfNetworkIODialogContent.is, VcfNetworkIODialogContent);
