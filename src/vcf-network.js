5a18712165ed41ae11deaa89404aa4e11058b138
Nii Yeboah <nii@vaadin.com>
2019-08-23 02:16:56 +0300

    Update README

--- src/vcf-network.js
+++ src/vcf-network.js
@@ -92,7 +92,7 @@ class VcfNetwork extends ElementMixin(ThemableMixin(PolymerElement)) {
   }
 
   static get version() {
-    return '1.1.0';
+    return '1.1.1';
   }
 
   static get properties() {
